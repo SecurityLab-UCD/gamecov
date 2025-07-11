57479e805b9bab29124ce97be2b18e7a808c173a
Yifeng He <yfhe.prsn@gmail.com>
2025-07-11 14:04:06 -0700

    feat: add image dedup

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 import os
 import argparse
 import mapcov
-from mapcov.loader import load_mp4, load_mp4_lazy
+from mapcov import load_mp4, hash_dedup, ssim_dedup
 
 
 def main():
     """placeholder main function to ensure the script can be run."""
+    images = load_mp4("assets/videos/38118.mp4")
 
-    pass
+    print(len(images))
+    
+    # Deduplicate using hash method
+    unique_images_hash = hash_dedup(images)
+    print(f"Unique images using hash deduplication: {len(unique_images_hash)}")
+    
+    # Deduplicate using SSIM method
+    unique_images_ssim = ssim_dedup(images)
+    print(f"Unique images using SSIM deduplication: {len(unique_images_ssim)}")
 
 
 if __name__ == "__main__":
