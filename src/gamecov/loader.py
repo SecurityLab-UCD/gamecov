3745903a2c49fbac54d19ae4fd1787a114164de5
Yifeng He <yfhe.prsn@gmail.com>
2025-07-23 15:34:48 -0700

    feat: get game cov (#8)
    * add get_frame_cov
    
    * update package export
    
    * fix: video extension starts with .
    
    * remove assert in load_map4_lazy
    
    * upgrade returns
    
    * fix mypy and pylint ci

--- src/gamecov/loader.py
+++ src/gamecov/loader.py
@@ -1,16 +1,17 @@
 from typing import Generator
 
 import imageio.v3 as iio
+from returns.result import safe
 
 from .frame import Frame
+from .frame_cov import FrameCoverage
 
 
 def load_mp4(url: str) -> list[Frame]:
     """Load an MP4 file as a list of Frames."""
-    assert url.endswith(".mp4"), "File must be an MP4 file"
     # bulk read all frames
     # Warning: large videos will consume a lot of memory (RAM)
-    frames = iio.imread(url, plugin="pyav")
+    frames = iio.imread(url, plugin="pyav", extension=".mp4")
     return [Frame.fromarray(f) for f in frames]
 
 
@@ -18,7 +19,13 @@ def load_mp4_lazy(url: str) -> Generator[Frame, None, None]:
     """Load an MP4 file as a generator of Frames.
     for large videos, use this to avoid high memory usage.
     """
-    assert url.endswith(".mp4"), "File must be an MP4 file"
     # iterate over large videos
-    for frame in iio.imiter(url, plugin="pyav"):
+    for frame in iio.imiter(url, plugin="pyav", extension=".mp4"):
         yield Frame.fromarray(frame)
+
+
+@safe
+def get_frame_cov(url: str) -> FrameCoverage:
+    """Get the frame coverage for a given MP4 file."""
+    frames = load_mp4(url)
+    return FrameCoverage(frames)
