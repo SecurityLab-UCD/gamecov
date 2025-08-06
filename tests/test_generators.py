fa938cfbbdbf8a868c3f63e1e86f95724fdcc462
Yifeng He <heeafen0602@gmail.com>
2025-08-06 16:21:06 -0700

    check dedup consistancy of generators

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,20 +1,29 @@
 from gamecov import Frame
-from gamecov.generator import frames, frames_lists
+import gamecov.generator as cg
+from gamecov.dedup import hash_dedup
 
 
 from hypothesis import given, strategies as st
 
 
-@given(frame=frames(height=128, width=128, channels=3))
+@given(frame=cg.frames(height=128, width=128, channels=3))
 def test_size_is_right(frame: Frame):
     assert frame.img.size == (128, 128)
     assert frame.img.mode == "RGB"  # 3-channel check
 
 
-@given(frames=frames_lists)
+@given(frames=cg.frames_lists)
 def test_frames_list(frames: list[Frame]):
-    assert len(frames) >= 3
-    assert len(frames) <= 100
+    assert len(frames) >= 5
+    assert len(frames) <= 50
     for frame in frames:
         assert frame.img.size == (128, 128)
         assert frame.img.mode == "RGB"
+
+
+@given(frames=cg.frames_lists)
+def test_rand_dedup(frames: list[Frame]):
+    assert len(hash_dedup(frames)) <= len(frames), "Deduplication failed"
+    assert len(hash_dedup(frames)) == len(
+        set(hash_dedup(frames))
+    ), "Hash deduplication failed uniqueness check"
