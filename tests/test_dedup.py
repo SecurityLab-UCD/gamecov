ea3f26e07d0c05edff135e76448abae30442e34c
Yifeng He <heeafen0602@gmail.com>
2025-08-07 17:17:15 -0700

    add test for dedup

---
+++ tests/test_dedup.py
@@ -0,0 +1,16 @@
+from gamecov import Frame
+from gamecov.dedup import hash_dedup
+import gamecov.generator as cg
+from hypothesis import given, settings
+from hypothesis.stateful import RuleBasedStateMachine, rule
+from hypothesis import strategies as st
+
+
+@given(data=st.data(), n=st.integers(min_value=1, max_value=100))
+def test_dedup_update(data, n):
+    all_frames: set[Frame] = set()
+    for _ in range(n):
+        frames = data.draw(cg.frames_lists)
+        prev_len = len(all_frames)
+        all_frames.update(hash_dedup(frames))
+        assert len(all_frames) >= prev_len
